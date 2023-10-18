1fd5ac558c6069f1808884d44281ca0368b77d76
nicobv01 <nicolbatista011@gmail.com>
2023-10-18 13:39:36 -0400

    add Authorize attribute

--- PokeDek.API/Program.cs
+++ PokeDek.API/Program.cs
@@ -7,7 +7,7 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services.AddAuthentication();
+builder.Services.AddAuthentication().AddJwtBearer();
 builder.Services.AddAuthorization();
 
 var app = builder.Build();
