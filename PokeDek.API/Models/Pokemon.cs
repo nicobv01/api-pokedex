bfc2ea35e71e3616925458e7b2bcccff8a01b200
nicobv01 <nicolbatista011@gmail.com>
2023-10-18 09:09:55 -0400

    add model Pokemon

---
+++ PokeDek.API/Models/Pokemon.cs
@@ -0,0 +1,11 @@
+﻿namespace PokeDek.API.Models
+{
+    public class Pokemons
+    {
+        public string Code { get; set; }
+        public string ImageUrl { get; set; }
+        public string Name { get; set; }
+        public string Type1 { get; set; }
+        public string Type2 { get; set; }
+    }
+}
