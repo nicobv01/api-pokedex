c86c68af008be3d31fb2f5e2959faf15b8666d21
nicobv01 <nicolbatista011@gmail.com>
2023-10-18 09:02:37 -0400

    add PokemonsController

---
+++ PokeDek.API/Controllers/PokemonsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PokeDek.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PokemonsController : Controller
+    {
+
+
+    }
+}
